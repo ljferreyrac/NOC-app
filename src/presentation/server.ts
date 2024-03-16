2811f3b5f5154ac287b0a493a792c4df64207fda
ljferreyrac <lferreyrac04@hotmail.com>
2024-03-15 21:48:49 -0500

    First Approach NOC App

---
+++ src/presentation/server.ts
@@ -0,0 +1,18 @@
+import { CheckService } from "../domain/use-cases/checks/check-server";
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+    public static start () {
+        console.log('Server started...');
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'http://google.com'
+                new CheckService(
+                    () => console.log(`${url} is ok`),
+                    ( error ) => console.log( error )
+                ).execute(url);
+            }
+        );
+    }
+}
